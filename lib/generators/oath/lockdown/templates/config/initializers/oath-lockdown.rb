4caa7b9705f9575e6d580962d5b325b9f0491248
Justin Tomich <tomichj@gmail.com>
2018-07-14 17:18:10 -0700

    Added comments and some reasonable defaults to generator's config template

--- lib/generators/oath/lockdown/templates/config/initializers/oath-lockdown.rb
+++ lib/generators/oath/lockdown/templates/config/initializers/oath-lockdown.rb
@@ -1,20 +1,30 @@
 Oath.configure do |config|
-  config.http_authenticatable = true
-  config.http_authenticatable_on_xhr = true
-  config.http_authentication_realm = 'Application'
+  # Enable 'brute force' protection - lock accounts on repeated fails.
+  #
+  # config.max_consecutive_bad_logins_allowed = 4
+  # config.bad_login_lockout_period = 15.minutes
 
-  config.max_consecutive_bad_logins_allowed = 4
-  config.bad_login_lockout_period = 15.minutes
+  # Expire idle sessions out.
+  #
+  # config.timeout_in = 15.minutes
 
-  config.timeout_in = 15.minutes
+  # Expire sessions after they'be been logged in for too long.
+  #
+  # config.max_session_lifetime = 8.hours
 
-  config.max_session_lifetime = 8.hours
+  # Record a few basic facts about the user - IP address, login time.
+  #
+  # config.track_user = false
 
-  config.track_user = false
-
-  # config.rotate_csrf_on_sign_in = true
-
+  # Allow 'remember me' settings.
+  #
   # config.remember_for = 2.weeks
-  # config.remember_cookie_name = 'remember_user_key'
   # config.expire_remember_me_on_sign_out = true
+  # config.remember_cookie_name = 'remember_user_key'
+
+  # Http basic authentication
+  #
+  # config.http_authenticatable = false
+  # config.http_authenticatable_on_xhr = true
+  # config.http_authentication_realm = 'Application'
 end
--- spec/features/strategy/remember_me_spec.rb
+++ spec/features/strategy/remember_me_spec.rb
@@ -1,23 +1,31 @@
 require 'spec_helper'
 
 describe 'User signs in', type: :feature do
-  context 'with with remember_me' do
-    before do
-      User.create!(email: 'example@example.com', password_digest: 'password')
+  context 'with remember_me requested' do
+    it 'will assign remember_me cookie' do
+      user_signs_in_with_remember_me
+      expect(remember_cookie).to_not be_nil
     end
-
-    it 'signs in' do
-      visit sign_in_path
-      fill_in 'session[email]', with: 'example@example.com'
-      fill_in 'session[password]', with: 'password'
-      check 'session[remember_me]'
-      click_button 'go'
-
-      visit sign_in_path
-
-      # already signed in, so you get forwarded to your root path
-      expect(current_path).to eq posts_path
-    end
-
   end
+
+  context 'with remember_me cookie set' do
+    it 'is authenticated with remember_me cookie' do
+      user_signs_in_with_remember_me
+      visit sign_in_path
+      expect(current_path).to eq root_path # remember_me forwards to root path
+    end
+  end
+end
+
+def user_signs_in_with_remember_me
+  User.create!(email: 'example@example.com', password_digest: 'password')
+  visit sign_in_path
+  fill_in 'session[email]', with: 'example@example.com'
+  fill_in 'session[password]', with: 'password'
+  check 'session[remember_me]'
+  click_button 'go'
+end
+
+def remember_cookie
+  Capybara.current_session.driver.request.cookies['remember_user_key']
 end
